5b5797547630fe82bccc033761a7c487a220e3e5
Esther Kang <eskang87@gmail.com>
2021-12-10 16:05:10 -0800

    commit tree 6510f19c0d5d99614260b63e2898eb9609f58cd4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+  const txt = document.getElementById('text');
+  txt.textContent = 'This is really cool!'
+}
+);
+
+/* console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+); */
+
+//Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
